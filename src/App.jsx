6a941e7d8eb29f3220bac23f86bf068dd53dc785
farhan khan <farhankhanpro3@gmail.com>
2024-11-08 09:28:14 +0530

    add main page to contain at one place

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useQNA } from './store/store';
 import Add from './components/Add';
+import CartMain from './pages/CartMain';
 const App = () => {
     const {question,ans} = useQNA();
     console.log(question)
     console.log(ans)
   return (
     <div>
-     {/* <InputCart/> */}
-     <Add/>
+    <CartMain/>
     </div>
   )
 }
